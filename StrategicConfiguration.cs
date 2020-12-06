e6c3d8e3798e2f438f789078dbcc6a8d48034581
scriptmaster <msheriffusa@gmail.com>
2020-12-06 13:33:23 +0530

    Strategic Data

---
+++ StrategicConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace StrategicData
+{
+    public class StrategicConfiguration
+    {
+        public IConfigurationRoot Configuration { get; }
+
+        public StrategicConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json");
+
+            Configuration = builder.Build();
+
+            Console.WriteLine($"configuration: {Configuration.GetConnectionString("sqlserver")}");
+        }
+
+        public void ReadConfiguration()
+        {
+            // 
+        }
+
+    }
+}
