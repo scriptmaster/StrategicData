ad8b2a522c0c356d0372a7a7573bdff96356296e
scriptmaster <msheriffusa@gmail.com>
2020-12-06 17:10:36 +0530

    Check log

--- AppDBContext.cs
+++ AppDBContext.cs
@@ -7,38 +7,15 @@ namespace StrategicData
 {
     public class AppDbContext : DbContext
     {
-        private readonly string connectionString;
-
-        public DBProvider DBProvider { get; set; } = DBProvider.InMemory;
-
-        public AppDbContext(string connectionString)
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
         {
-            this.connectionString = connectionString;
+            // 
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            _ = DBProvider switch
-            {
-                DBProvider.SQLServer => optionsBuilder.UseSqlServer(connectionString),
-                DBProvider.PGSQL => optionsBuilder.UseNpgsql(connectionString),
-                DBProvider.MySQL => optionsBuilder.UseMySQL(connectionString),
-                DBProvider.SQLite => optionsBuilder.UseSqlite(connectionString),
-                _ => optionsBuilder.UseInMemoryDatabase(connectionString)
-            };
+            // 
         }
     }
-
-    public enum DBProvider
-    {
-        InMemory,
-        SQLServer,
-        PGSQL,
-        Oracle,
-        MySQL,
-        SQLite,
-        MongoDB,
-        Cassandra
-    }
-
 }
